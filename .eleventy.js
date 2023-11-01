d2e77843ca2b07325eae5d3eb1c7b17beab6d3dc
Veronika Holá <veronika.hola@icloud.com>
2023-11-01 12:15:41 +0100

    eleventy

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+module.exports = function(eleventyConfig) {
+
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy("images");
+
+	// Zkopírovat css/ to _site/css/
+	eleventyConfig.addPassthroughCopy("css");
+
+//   eleventyConfig.addPassthroughCopy("glide.min.js")
+
+	// Zkopírovat všechny *.js soubory na stejné umístění do _site
+	// eleventyConfig.addPassthroughCopy("**/*.js");
+
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
