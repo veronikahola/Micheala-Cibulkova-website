f65d025a47d483276981764f69e511a9a68a8f45
Veronika Holá <veronika.hola@icloud.com>
2023-12-17 16:57:02 +0100

    siema

--- togglenabidka.js
+++ togglenabidka.js
@@ -62,6 +62,64 @@
 
 
    
+// document.addEventListener('DOMContentLoaded', function() {
+//     const toggleCheckbox = document.querySelector('.toggle__checkbox');
+//     const toggleTextPsi = document.querySelector('.toggle__text--psi');
+//     const toggleTextLide = document.querySelector('.toggle__text--lide');
+//     const nabidkaPsi = document.querySelector('.nabidka--psi');
+//     const nabidkaLide = document.querySelector('.nabidka--lide');
+
+//     // Function to initialize Siema for a given section
+//     function initializeSiema(section, perPageValue) {
+//         const siemaElement = section.querySelector('.siema');
+
+//         if (siemaElement && !siemaElement.siemaInitialized) {
+//             new Siema({
+//                 selector: siemaElement,
+//                 perPage: {
+//                     100: 1,
+//                     768: perPageValue,
+//                 },
+//             });
+
+//             // Mark Siema as initialized
+//             siemaElement.siemaInitialized = true;
+//         }
+//     }
+
+//     toggleCheckbox.addEventListener('change', function() {
+//         if (this.checked) {
+//             toggleTextPsi.style.color = '#B3A8A8';
+//             toggleTextLide.style.color = '#685B5E';
+//             nabidkaPsi.style.display = 'none';
+//             nabidkaLide.style.display = 'block';
+
+//             // Check if Siema instance for nabidkaLide is not initialized
+//             if (!nabidkaLide.querySelector('.siema').siemaInitialized) {
+//                 initializeSiema(nabidkaLide, 2); // Example: 2 slides for nabidkaLide
+//             }
+//         } else {
+//             toggleTextPsi.style.color = '#685B5E';
+//             toggleTextLide.style.color = '#B3A8A8';
+//             nabidkaPsi.style.display = 'block';
+//             nabidkaLide.style.display = 'none';
+
+//             // Check if Siema instance for nabidkaPsi is not initialized
+//             if (!nabidkaPsi.querySelector('.siema').siemaInitialized) {
+//                 initializeSiema(nabidkaPsi, 3); // Example: 3 slides for nabidkaPsi
+//             }
+//         }
+//     });
+
+//     // Initialize Siema for both sections on page load
+//     initializeSiema(nabidkaPsi, 3);  // Example: 3 slides for nabidkaPsi
+//     initializeSiema(nabidkaLide, 2); // Example: 2 slides for nabidkaLide
+// });
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const toggleCheckbox = document.querySelector('.toggle__checkbox');
     const toggleTextPsi = document.querySelector('.toggle__text--psi');
@@ -87,28 +145,35 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    // Function to handle visibility change and initialize Siema if needed
+    function handleVisibilityChange() {
+        if (document.visibilityState === 'visible') {
+            if (toggleCheckbox.checked) {
+                initializeSiema(nabidkaLide, 2); // Example: 2 slides for nabidkaLide
+            } else {
+                initializeSiema(nabidkaPsi, 3); // Example: 3 slides for nabidkaPsi
+            }
+        }
+    }
+
+    // Attach event listener for visibility change
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
     toggleCheckbox.addEventListener('change', function() {
         if (this.checked) {
             toggleTextPsi.style.color = '#B3A8A8';
             toggleTextLide.style.color = '#685B5E';
             nabidkaPsi.style.display = 'none';
             nabidkaLide.style.display = 'block';
-
-            // Check if Siema instance for nabidkaLide is not initialized
-            if (!nabidkaLide.querySelector('.siema').siemaInitialized) {
-                initializeSiema(nabidkaLide, 2); // Example: 2 slides for nabidkaLide
-            }
         } else {
             toggleTextPsi.style.color = '#685B5E';
             toggleTextLide.style.color = '#B3A8A8';
             nabidkaPsi.style.display = 'block';
             nabidkaLide.style.display = 'none';
-
-            // Check if Siema instance for nabidkaPsi is not initialized
-            if (!nabidkaPsi.querySelector('.siema').siemaInitialized) {
-                initializeSiema(nabidkaPsi, 3); // Example: 3 slides for nabidkaPsi
-            }
         }
+
+        // Trigger visibility change handling after toggle change
+        handleVisibilityChange();
     });
 
     // Initialize Siema for both sections on page load
