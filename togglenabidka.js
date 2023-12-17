51ca5bc8ef339f888179791cf9e971d08c1587bc
Veronika Holá <veronika.hola@icloud.com>
2023-12-17 15:54:21 +0100

    siema

--- togglenabidka.js
+++ togglenabidka.js
@@ -2,10 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const toggleCheckbox = document.querySelector('.toggle__checkbox');
     const toggleTextPsi = document.querySelector('.toggle__text--psi');
     const toggleTextLide = document.querySelector('.toggle__text--lide');
-
     const nabidkaPsi = document.querySelector('.nabidka--psi');
     const nabidkaLide = document.querySelector('.nabidka--lide');
 
+
+    nabidkaLide.style.display = 'none';
+    nabidkaPsi.style.display = 'block';
+    toggleTextPsi.style.color = '#685B5E';
+    toggleTextLide.style.color = '#B3A8A8';
+    toggleCheckbox.checked = false;
+    
     
 
     toggleCheckbox.addEventListener('change', function() {
@@ -17,6 +23,8 @@ document.addEventListener('DOMContentLoaded', function() {
             toggleTextLide.style.color = '#685B5E';
             nabidkaPsi.style.display = 'none';
             nabidkaLide.style.display = 'block';
+
+
         } else {
             toggleTextPsi.style.transition = 'color 0.5s';
             toggleTextLide.style.transition = 'color 0.5s';
@@ -28,21 +36,27 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    toggleTextPsi.addEventListener('click', function() {
-        toggleCheckbox.checked = false;
-        toggleCheckbox.dispatchEvent(new Event('change'));
-    });
-
-    toggleTextLide.addEventListener('click', function() {
-        toggleCheckbox.checked = true;
-        toggleCheckbox.dispatchEvent(new Event('change'));
-    });
+    
 
 
-    nabidkaLide.style.display = 'none';
-    nabidkaPsi.style.display = 'block';
-    toggleTextPsi.style.color = '#685B5E';
-    toggleTextLide.style.color = '#B3A8A8';
-    toggleCheckbox.checked = false;
+    
     
 });
+
+
+
+// toggleTextPsi.addEventListener('click', function() {
+    //     toggleCheckbox.checked = false;
+    //     toggleCheckbox.dispatchEvent(new Event('change'));
+    // });
+
+    // toggleTextLide.addEventListener('click', function() {
+    //     toggleCheckbox.checked = true;
+    //     toggleCheckbox.dispatchEvent(new Event('change'));
+    // });
+
+    // nabidkaLide.style.display = 'none';
+    // nabidkaPsi.style.display = 'block';
+    // toggleTextPsi.style.color = '#685B5E';
+    // toggleTextLide.style.color = '#B3A8A8';
+    // toggleCheckbox.checked = false;
