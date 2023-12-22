b816a778df4bc561d6e5f0f984dacc928ce68338
Veronika Holá <veronika.hola@icloud.com>
2023-12-22 10:55:36 +0100

    hammer swipe

--- togglenabidka.js
+++ togglenabidka.js
@@ -1,40 +1,103 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var slickInstance;
 
     function initializeSlick() {
-      slickInstance = $('.carousel').slick({
-        slidesToShow: 1,
-        dots: true,
-        arrows: false,
-        adaptiveHeight: true,
-      });
+        slickInstance = $('.carousel').slick({
+            slidesToShow: 1,
+            dots: true,
+            arrows: false,
+            adaptiveHeight: true,
+        });
     }
 
     function destroySlick() {
-      $('.carousel').slick('unslick');
+        $('.carousel').slick('unslick');
     }
 
     // Initial setup
     if (window.innerWidth <= 768) {
-      initializeSlick();
+        initializeSlick();
     }
 
     // Toggle button change event
-    $('#toggle').change(function() {
-      destroySlick();
-      if (window.innerWidth <= 768) {
-        initializeSlick();
-      }
+    $('#toggle').change(function () {
+        destroySlick();
+        if (window.innerWidth <= 768) {
+            initializeSlick();
+        }
     });
 
     // Window resize event
-    $(window).resize(function() {
-      destroySlick();
-      if (window.innerWidth <= 768) {
-        initializeSlick();
-      }
+    $(window).resize(function () {
+        destroySlick();
+        if (window.innerWidth <= 768) {
+            initializeSlick();
+        }
     });
-  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleCheckbox = document.querySelector('.toggle__checkbox');
+    const toggleTextPsi = document.querySelector('.toggle__text--psi');
+    const toggleTextLide = document.querySelector('.toggle__text--lide');
+    const nabidkaPsi = document.querySelector('.nabidka--psi');
+    const nabidkaLide = document.querySelector('.nabidka--lide');
+
+    nabidkaLide.style.display = 'none';
+    nabidkaPsi.style.display = 'block';
+    toggleTextPsi.style.color = '#685B5E';
+    toggleTextLide.style.color = '#B3A8A8';
+    toggleCheckbox.checked = false;
+
+    // Function to handle toggle switch change
+    function handleToggleChange() {
+        if (toggleCheckbox.checked) {
+            toggleTextPsi.style.color = '#B3A8A8';
+            toggleTextLide.style.color = '#685B5E';
+            nabidkaPsi.style.display = 'none';
+            nabidkaLide.style.display = 'block';
+        } else {
+            toggleTextPsi.style.color = '#685B5E';
+            toggleTextLide.style.color = '#B3A8A8';
+            nabidkaPsi.style.display = 'block';
+            nabidkaLide.style.display = 'none';
+        }
+    }
+
+    // Initialize Hammer.js on the toggle switch container
+    const toggleContainer = document.querySelector('.toggle__container');
+    const hammer = new Hammer(toggleContainer);
+
+    // Add swipe event listeners
+    hammer.on('swiperight', function () {
+        toggleCheckbox.checked = true;
+        handleToggleChange();
+        initializeSlick(); // Initialize slick after swipe
+    });
+
+    hammer.on('swipeleft', function () {
+        toggleCheckbox.checked = false;
+        handleToggleChange();
+        initializeSlick(); // Initialize slick after swipe
+    });
+
+    // Add click event listeners for text labels
+    toggleTextPsi.addEventListener('click', function () {
+        toggleCheckbox.checked = false;
+        handleToggleChange();
+        initializeSlick(); // Initialize slick after click
+    });
+
+    toggleTextLide.addEventListener('click', function () {
+        toggleCheckbox.checked = true;
+        handleToggleChange();
+        initializeSlick(); // Initialize slick after click
+    });
+
+    // Add change event listener for the checkbox
+    toggleCheckbox.addEventListener('change', handleToggleChange);
+});
+
 
 
 
@@ -76,63 +139,3 @@ $(document).ready(function(){
 //         toggleCheckbox.dispatchEvent(new Event('change'));
 //     });
 // });
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleCheckbox = document.querySelector('.toggle__checkbox');
-    const toggleTextPsi = document.querySelector('.toggle__text--psi');
-    const toggleTextLide = document.querySelector('.toggle__text--lide');
-    const nabidkaPsi = document.querySelector('.nabidka--psi');
-    const nabidkaLide = document.querySelector('.nabidka--lide');
-
-    nabidkaLide.style.display = 'none';
-    nabidkaPsi.style.display = 'block';
-    toggleTextPsi.style.color = '#685B5E';
-    toggleTextLide.style.color = '#B3A8A8';
-    toggleCheckbox.checked = false;
-
-    // Function to handle toggle switch change
-    function handleToggleChange() {
-        if (toggleCheckbox.checked) {
-            toggleTextPsi.style.color = '#B3A8A8';
-            toggleTextLide.style.color = '#685B5E';
-            nabidkaPsi.style.display = 'none';
-            nabidkaLide.style.display = 'block';
-        } else {
-            toggleTextPsi.style.color = '#685B5E';
-            toggleTextLide.style.color = '#B3A8A8';
-            nabidkaPsi.style.display = 'block';
-            nabidkaLide.style.display = 'none';
-        }
-    }
-
-    // Initialize Hammer.js on the toggle switch container
-    const toggleContainer = document.querySelector('.toggle__container');
-    const hammer = new Hammer(toggleContainer);
-
-    // Add swipe event listeners
-    hammer.on('swiperight', function () {
-        toggleCheckbox.checked = true;
-        handleToggleChange();
-    });
-
-    hammer.on('swipeleft', function () {
-        toggleCheckbox.checked = false;
-        handleToggleChange();
-    });
-
-    // Add click event listeners for text labels
-    toggleTextPsi.addEventListener('click', function () {
-        toggleCheckbox.checked = false;
-        handleToggleChange();
-    });
-
-    toggleTextLide.addEventListener('click', function () {
-        toggleCheckbox.checked = true;
-        handleToggleChange();
-    });
-
-    // Add change event listener for the checkbox
-    toggleCheckbox.addEventListener('change', handleToggleChange);
-});
