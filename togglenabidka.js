e066e673569fdaab578b26f49107a024d1b7c4b5
Veronika Holá <veronika.hola@icloud.com>
2023-12-22 08:43:56 +0100

    siema

--- togglenabidka.js
+++ togglenabidka.js
@@ -5,87 +5,11 @@
 //     const nabidkaPsi = document.querySelector('.nabidka--psi');
 //     const nabidkaLide = document.querySelector('.nabidka--lide');
 
-
 //     nabidkaLide.style.display = 'none';
 //     nabidkaPsi.style.display = 'block';
 //     toggleTextPsi.style.color = '#685B5E';
 //     toggleTextLide.style.color = '#B3A8A8';
 //     toggleCheckbox.checked = false;
-    
-    
-
-//     toggleCheckbox.addEventListener('change', function() {
-//         if (this.checked) {
-//             toggleTextPsi.style.transition = 'color 0.5s';
-//             toggleTextLide.style.transition = 'color 0.5s';
-        
-//             toggleTextPsi.style.color = '#B3A8A8';
-//             toggleTextLide.style.color = '#685B5E';
-//             nabidkaPsi.style.display = 'none';
-//             nabidkaLide.style.display = 'block';
-
-
-//         } else {
-//             toggleTextPsi.style.transition = 'color 0.5s';
-//             toggleTextLide.style.transition = 'color 0.5s';
-            
-//             toggleTextPsi.style.color = '#685B5E';
-//             toggleTextLide.style.color = '#B3A8A8';
-//             nabidkaPsi.style.display = 'block';
-//             nabidkaLide.style.display = 'none';
-//         }
-//     });
- 
-// });
-
-
-
-// toggleTextPsi.addEventListener('click', function() {
-    //     toggleCheckbox.checked = false;
-    //     toggleCheckbox.dispatchEvent(new Event('change'));
-    // });
-
-    // toggleTextLide.addEventListener('click', function() {
-    //     toggleCheckbox.checked = true;
-    //     toggleCheckbox.dispatchEvent(new Event('change'));
-    // });
-
-    // nabidkaLide.style.display = 'none';
-    // nabidkaPsi.style.display = 'block';
-    // toggleTextPsi.style.color = '#685B5E';
-    // toggleTextLide.style.color = '#B3A8A8';
-    // toggleCheckbox.checked = false;
-
-
-
-
-
-
-   
-// document.addEventListener('DOMContentLoaded', function() {
-//     const toggleCheckbox = document.querySelector('.toggle__checkbox');
-//     const toggleTextPsi = document.querySelector('.toggle__text--psi');
-//     const toggleTextLide = document.querySelector('.toggle__text--lide');
-//     const nabidkaPsi = document.querySelector('.nabidka--psi');
-//     const nabidkaLide = document.querySelector('.nabidka--lide');
-
-//     // Function to initialize Siema for a given section
-//     function initializeSiema(section, perPageValue) {
-//         const siemaElement = section.querySelector('.siema');
-
-//         if (siemaElement && !siemaElement.siemaInitialized) {
-//             new Siema({
-//                 selector: siemaElement,
-//                 perPage: {
-//                     100: 1,
-//                     768: perPageValue,
-//                 },
-//             });
-
-//             // Mark Siema as initialized
-//             siemaElement.siemaInitialized = true;
-//         }
-//     }
 
 //     toggleCheckbox.addEventListener('change', function() {
 //         if (this.checked) {
@@ -93,29 +17,24 @@
 //             toggleTextLide.style.color = '#685B5E';
 //             nabidkaPsi.style.display = 'none';
 //             nabidkaLide.style.display = 'block';
-
-//             // Check if Siema instance for nabidkaLide is not initialized
-//             if (!nabidkaLide.querySelector('.siema').siemaInitialized) {
-//                 initializeSiema(nabidkaLide, 2); // Example: 2 slides for nabidkaLide
-//             }
 //         } else {
 //             toggleTextPsi.style.color = '#685B5E';
 //             toggleTextLide.style.color = '#B3A8A8';
 //             nabidkaPsi.style.display = 'block';
 //             nabidkaLide.style.display = 'none';
-
-//             // Check if Siema instance for nabidkaPsi is not initialized
-//             if (!nabidkaPsi.querySelector('.siema').siemaInitialized) {
-//                 initializeSiema(nabidkaPsi, 3); // Example: 3 slides for nabidkaPsi
-//             }
 //         }
 //     });
 
-//     // Initialize Siema for both sections on page load
-//     initializeSiema(nabidkaPsi, 3);  // Example: 3 slides for nabidkaPsi
-//     initializeSiema(nabidkaLide, 2); // Example: 2 slides for nabidkaLide
-// });
+//     toggleTextPsi.addEventListener('click', function() {
+//         toggleCheckbox.checked = false;
+//         toggleCheckbox.dispatchEvent(new Event('change'));
+//     });
 
+//     toggleTextLide.addEventListener('click', function() {
+//         toggleCheckbox.checked = true;
+//         toggleCheckbox.dispatchEvent(new Event('change'));
+//     });
+// });
 
 
 
@@ -126,38 +45,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const toggleTextLide = document.querySelector('.toggle__text--lide');
     const nabidkaPsi = document.querySelector('.nabidka--psi');
     const nabidkaLide = document.querySelector('.nabidka--lide');
+    const siemas = document.querySelectorAll('.siema');
 
-    // Function to initialize Siema for a given section
-    function initializeSiema(section, perPageValue) {
-        const siemaElement = section.querySelector('.siema');
-
-        if (siemaElement && !siemaElement.siemaInitialized) {
-            new Siema({
-                selector: siemaElement,
-                perPage: {
-                    100: 1,
-                    768: perPageValue,
-                },
-            });
-
-            // Mark Siema as initialized
-            siemaElement.siemaInitialized = true;
-        }
-    }
-
-    // Function to handle visibility change and initialize Siema if needed
-    function handleVisibilityChange() {
-        if (document.visibilityState === 'visible') {
-            if (toggleCheckbox.checked) {
-                initializeSiema(nabidkaLide, 2); // Example: 2 slides for nabidkaLide
-            } else {
-                initializeSiema(nabidkaPsi, 3); // Example: 3 slides for nabidkaPsi
-            }
-        }
-    }
-
-    // Attach event listener for visibility change
-    document.addEventListener('visibilitychange', handleVisibilityChange);
+    nabidkaLide.style.display = 'none';
+    nabidkaPsi.style.display = 'block';
+    toggleTextPsi.style.color = '#685B5E';
+    toggleTextLide.style.color = '#B3A8A8';
+    toggleCheckbox.checked = false;
 
     toggleCheckbox.addEventListener('change', function() {
         if (this.checked) {
@@ -176,7 +70,58 @@ document.addEventListener('DOMContentLoaded', function() {
         handleVisibilityChange();
     });
 
-    // Initialize Siema for both sections on page load
-    initializeSiema(nabidkaPsi, 3);  // Example: 3 slides for nabidkaPsi
-    initializeSiema(nabidkaLide, 2); // Example: 2 slides for nabidkaLide
+    toggleTextPsi.addEventListener('click', function() {
+        toggleCheckbox.checked = false;
+        toggleCheckbox.dispatchEvent(new Event('change'));
+    });
+
+    toggleTextLide.addEventListener('click', function() {
+        toggleCheckbox.checked = true;
+        toggleCheckbox.dispatchEvent(new Event('change'));
+    });
+
+    // Function to handle visibility change and resize Siema if needed
+    function handleVisibilityChange() {
+        for (const siema of siemas) {
+            let perPageValue = 1;
+
+            if (siema.classList.contains('siema-3-slides')) {
+                perPageValue = 3;
+            } else if (siema.classList.contains('siema-2-slides')) {
+                perPageValue = 2;
+            }
+
+            // Check if the screen width is smaller than 1000px
+            if (window.innerWidth < 1000) {
+                siema.resize();
+            }
+        }
+    }
+
+    // Attach event listener for visibility change
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    // Initialize Siema for all elements with the 'siema' class
+    for (const siema of siemas) {
+        let perPageValue = 1;
+
+        if (siema.classList.contains('siema-3-slides')) {
+            perPageValue = 3;
+        } else if (siema.classList.contains('siema-2-slides')) {
+            perPageValue = 2;
+        }
+
+        initializeSiema(siema, perPageValue);
+    }
+
+    // Function to initialize Siema for a given section
+    function initializeSiema(section, perPageValue) {
+        new Siema({
+            selector: section,
+            perPage: {
+                100: 1,
+                768: perPageValue,
+            },
+        });
+    }
 });
